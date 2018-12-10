17b819d039b7c1bfe5266f93f062835cc00a1e5a
Saku Huuha <t7humi01@students.oamk.fi>
2018-12-10 13:46:03 +0200

    viimeinen versio

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 ﻿/*
 Konna-anturi
 5.11.2018
-Päivitetty 23.11.2018
+Päivitetty 10.12.2018
 Saku Huuha & Niko Hämäläinen
 */
 #include "mbed.h"
@@ -13,23 +13,17 @@ Timer timer;
 Ticker kulmatikkeri;
 Serial pc(USBTX,USBRX);
 L3G4200D gyro(D14, D15);
-int g[3]={0, 0, 0};
+int g[2]={0, 0};
 int gr[3]={0, 0, 0};
-double kulma[3] = {0.0 ,0.0 ,0.0};
+double kulma[2] = {0.0 ,0.0};
 
 double skaalaus = 0.06923;
-int aika = 0;
-double laskukulma = 0;
-double kulmaprint = 0;
-double testiarvo = 14.3;
-double xKulma = 0;
 AnalogIn xVal(A0);
 AnalogIn yVal(A1);
 AnalogIn zVal(A2);
-double x,y,z;
-double xr,yr,zr;
-double kulma_x,kulma_y,kulma_z;
-int laskuri = 0; 
+double x,y;
+double xr,yr;
+double kulma_x,kulma_y;
 
 AnalogIn lampo(A3);
 float Ur = 0;
@@ -38,17 +32,22 @@ float U0 = 3.3;
 float ntcR = 0;
 float lampoLukema = 0;
 float lampotila = 0;
-float apumuuttuja = 0;
+float painotus = 0;
+
+/*
+Kulmien laskenta suoritetaan keskeytyksissa Tickerin avulla. 
+Taustalla rajoittavana tekijana digitaalisesti lukemia antava gyrosensori vrt. analogisesti luettavaan kiihtyvyyssensoriin.
+Tickerilla varmistetaan, etta gyrosensorista luetaan oikeita arvoja, eika painotus heittele koodin suoritusajan tai digitaalisen mittauksen vuoksi
+*/
 
 void kulmatikkeri_isr(void)
 {
 
-
+//asetetaan gyrosensorin arvot muuttujiin
 g[0] = gr[0];
 g[1] = gr[1];
-g[2] = gr[2];
 
-// kiihtyvyysanturin arvot
+// kiihtyvyysanturin arvot muuttujiin ja muunnos kiihtyvyydeksi kalibroinnin perusteella
 
 x = xr;
 //x = x * 0.0015-48.466;
@@ -56,22 +55,24 @@ x = 0.0015 *  x - 48.83;
 
 y= yr;
 y = y* 0.0015 - 48.79;
-z = zr;
-z = 0.0015 * z - 49.813;
+
 //x = xVal;
 //x = x * 0.0103 + 0.4987;
 
+
+//asetetaan kiihtyvyksien maksimit ja minimit
 if(x > 9.81){x = 9.81;}
 if(y > 9.81){y = 9.81;}
-if(z > 9.81){z = 9.81;}
+
 
 if(x < -9.81){x = -9.81;}
 if(y < -9.81){y = -9.81;}
-if(z < -9.81){z = -9.81;}
 
 
 
-//kulma_x laskut
+
+//kulma_x laskut kiihtyvyyden osalta
+//kiihtyvyyden kulma x on gyron ja lopullisen kulman y
  
 if (kulma[1] > 90 && kulma [1] < 270)
 {
@@ -107,7 +108,8 @@ kulma_x = -asin(x /9.8101) * 180.0 / 3.141592;
 
 
 
-//kulma_y laskut 
+//kulma_y laskut kiihtyvyyden osalta
+//kiihtyvyyden kulma y on gyron ja lopullisen kulman x
 
 if(kulma[0] > 90 && kulma[0] < 270)
  { 
@@ -140,19 +142,14 @@ else
 
 
 
-
-kulma_z = asin(z / 9.8101) * 180.0 / 3.141592;
-
- 
+//gyro-arvon korjaus kalibroinnin perusteella
 
 
                 g[0] =g[0]- 10 ;
 
                 g[1] =g[1] + 1.1 ;
 
-                g[2] =g[2] -1.05 ;
-
-	 
+//lasketaan kulmat gyroarvojen ja kiihtyvyysarvojen perusteella	 
 		
 
 kulma[0]+= g[0] * 0.025 * skaalaus + 0.01*(kulma_y-kulma[0]);
@@ -162,6 +159,7 @@ kulma[0]+= g[0] * 0.025 * skaalaus + 0.01*(kulma_y-kulma[0]);
 
 kulma[1] += g[1] * 0.025 * skaalaus + 0.01*(kulma_x-kulma[1]); 
 
+//pysytaan -360 ja +360 asteen valilla
 
 if ( kulma [0] > 360||kulma[0] < -360)
 {
@@ -172,18 +170,10 @@ if(kulma [1] > 360||kulma[1] < -360)
 kulma[1] = 0;
 }
  
-
-                if(testiarvo==14.3)
-		{
-			testiarvo = 666.6;
-		}
-		else if(testiarvo==666.6)
-		{
-			testiarvo = 14.3;
-		}
                
 }
 
+// lampotilan laskemiseen funktiot excelista. kaytetaan yhta(aaripaissa) tai kahta funktiota, painotettuna sensoriarvon perusteella, laskemaan lampotila
 void lampotilalaskut()
 {
 lampoLukema = lampo.read_u16();
@@ -201,8 +191,8 @@ if(ntcR >= 49.29905 && ntcR < 130.156865)
 {
    //lampotila = -21.62*(log(ntcR)) + 71.969;
 
-   apumuuttuja=(ntcR-49.29905 )/(130.156865-49.29905 );
-   lampotila = (1-apumuuttuja)*(-21.62*(log(ntcR)) + 71.969) +(apumuuttuja) *(2*(pow(10,-8))*(pow(ntcR,4))+(2*(pow(10,-5)))*(pow(ntcR , 3))+(0.0052*(pow(ntcR,2)))- 0.7976*ntcR+ 16.014);
+   painotus=(ntcR-49.29905 )/(130.156865-49.29905 );
+   lampotila = (1-painotus)*(-21.62*(log(ntcR)) + 71.969) +(painotus) *(2*(pow(10,-8))*(pow(ntcR,4))+(2*(pow(10,-5)))*(pow(ntcR , 3))+(0.0052*(pow(ntcR,2)))- 0.7976*ntcR+ 16.014);
     
 }
 
@@ -210,9 +200,9 @@ if(ntcR >=20.3535 && ntcR < 49.29905 )
 {
   //lampotila = -25.93*(log(ntcR)) + 86.273;
 
-apumuuttuja = (ntcR-20.3535)/(49.29905 -20.3535);
+painotus = (ntcR-20.3535)/(49.29905 -20.3535);
 
-lampotila = (1-apumuuttuja)*(-25.93*(log(ntcR)) + 86.273) + (apumuuttuja)*(lampotila = -21.62*(log(ntcR)) + 71.969);
+lampotila = (1-painotus)*(-25.93*(log(ntcR)) + 86.273) + (painotus)*(lampotila = -21.62*(log(ntcR)) + 71.969);
 
 }
 
@@ -221,9 +211,9 @@ if (ntcR >=9.212 && ntcR < 20.3535)
   //lampotila = -0.0195 *(pow(ntcR,3)) + 0.7932
 //*(pow(ntcR,2))-12.288 * ntcR + 88.058;
 
-apumuuttuja = (ntcR-9.212)/(20.3535-9.212);
-lampotila = (1-apumuuttuja)*(-0.0195 *(pow(ntcR,3)) + 0.7932
-*(pow(ntcR,2))-12.288 * ntcR + 88.058 )+(apumuuttuja)* (-25.93*(log(ntcR)) + 86.273);
+painotus = (ntcR-9.212)/(20.3535-9.212);
+lampotila = (1-painotus)*(-0.0195 *(pow(ntcR,3)) + 0.7932
+*(pow(ntcR,2))-12.288 * ntcR + 88.058 )+(painotus)* (-25.93*(log(ntcR)) + 86.273);
 
 
 }
@@ -231,8 +221,8 @@ lampotila = (1-apumuuttuja)*(-0.0195 *(pow(ntcR,3)) + 0.7932
 if (ntcR < 9.212 && ntcR >=4.2795)
 {
  //lampotila = -28.36*(log(ntcR)) + 89.648;
-  apumuuttuja = (ntcR-4.2795)/(9.212-4.2795);
-lampotila = (1-apumuuttuja)*(-28.36*(log(ntcR)) + 89.648)+ (apumuuttuja)* (-0.0195 *(pow(ntcR,3)) + 0.7932
+  painotus = (ntcR-4.2795)/(9.212-4.2795);
+lampotila = (1-painotus)*(-28.36*(log(ntcR)) + 89.648)+ (painotus)* (-0.0195 *(pow(ntcR,3)) + 0.7932
 *(pow(ntcR,2))-12.288 * ntcR + 88.058);
 
 
@@ -250,8 +240,8 @@ if(ntcR< 4.2795 )
 
 int main() 
 {
-
-	kulmatikkeri.attach(&kulmatikkeri_isr, TS); 
+//kiinnitetaan Ticker
+kulmatikkeri.attach(&kulmatikkeri_isr, TS); 
 timer.start();
 timer.reset();
 
@@ -259,12 +249,13 @@ timer.reset();
 
 	while(true)
 	{
+//luetaan sensoriarvot
 gyro.read(gr);
 xr = xVal.read_u16();
 yr = yVal.read_u16();
-zr = zVal.read_u16();
 	
-if(timer.read_ms() >= 200)
+//tulostetaan arvot sarjaportille noin 250 ms valein
+if(timer.read_ms() >= 250)
 {
 lampotilalaskut();
 pc.printf("%7.2f, %7.2f, %7.2f\n" ,lampotila,kulma[0],kulma[1]);
